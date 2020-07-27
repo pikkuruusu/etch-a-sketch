34373cef20bde44418b266017d054d6a54a86a2d
Staffan Småros <staffan.smaros@gmail.com>
2020-07-27 16:32:18 +0300

    Added reset and resize board functionality

--- script.js
+++ script.js
@@ -12,12 +12,32 @@ function drawBox(squareCount) {
     }
 
     const sketchBlock = document.querySelectorAll('.sketch-block');
+    sketchBlock.forEach(block => block.style.backgroundColor = 'rgba(255,255,255,1');
+
     sketchBlock.forEach(block => block.addEventListener('mouseover', () => {
-        block.style.backgroundColor = 'black';
+        block.style.backgroundColor = 'rgba(0,0,0,1)';
     }));
+}
+
+function reset() {
+    let squareCount = prompt('How many boxes wide should we make the grid? (1-100)');
+    while (true) {
+        if (squareCount === null) {
+            return;
+        }
+        squareCount = parseInt(squareCount, 10);
+        if (!Number.isInteger(squareCount) || squareCount < 1 || squareCount > 100) {
+            squareCount = prompt('Yeah, that is not a good number of boxes. Try something between one and a hundred.')
+        } else {
+            break;
+        }
+    }
+    drawBox(squareCount);
 }
 
 const randomColorMap = ['FFADAD', 'FFD6A5', 'FDFFB6', 'CAFFBF', '9BF6FF', 'A0C4FF', 'BDB2FF', 'FFC6FF'];
 
+const resetBtn = document.querySelector('#resetBtn');
+resetBtn.addEventListener('click', reset);
 
 drawBox(16); // 100 seems like a good max
